f220718b7911f1bdca35eed60a8b8d6fee9e1b28
Benoit Parmentier <parmentier@nceas.ucsb.edu>
2013-09-16 10:45:00 -0700

    modis processing workflow: adding calls to modis download function, modis import and QC processing

--- Reading_MODIS_LST_with_QC.R
+++ Reading_MODIS_LST_with_QC.R
@@ -7,7 +7,7 @@
 #Much of the inspiration and code originates from Steve Mosher:
 #http://stevemosher.wordpress.com/2012/12/05/modis-qc-bits/
 #AUTHOR: Benoit Parmentier                                                                       
-#DATE: 09/11/2013                                                                                
+#DATE: 09/16/2013                                                                                
 #PROJECT: Space Time project and NCEAS                                 
 ###################################################################################################
 
@@ -30,7 +30,7 @@ infile_LC<-"MCD12Q1A2001001.h10v09.051.2012157220925.hdf"
 infile_LST <- list.files(path=in_dir,pattern=".*.hdf$")
 infile_IDRISI <- list.files(path=in_dir,pattern=".*.rst$")  
 
-function_analyses_paper <-"MODIS_and_raster_processing_functions_09112013.R"
+function_analyses_paper <-"MODIS_and_raster_processing_functions_09162013.R"
 script_path<-in_dir #path to script functions
 source(file.path(script_path,function_analyses_paper)) #source all functions used in this script.
 
@@ -50,8 +50,11 @@ str(GDALinfo_hdf)
 modis_subdataset <- attributes(GDALinfo_hdf)$subdsmdata
 print(modis_subdataset)
 
-###### PART I: Reading LST and Land cover layers (subset) ######
+###### PART I: EXPLORATION OF MODIS HDF PRODUCT: IMPORT, SCREENING OF QC FLAGS
 
+## First: Reading LST and Land cover layers (subset) ######
+
+#modis_subset_layer_LST_Day <- paste("HDF4_EOS:EOS_GRID:",hdf_file,":MODIS_Grid_Daily_1km_LST:LST_Day_1km",sep="")
 #HDF4_EOS:EOS_GRID:"MOD11A1.A2001001.h09v04.005.2006343034412.hdf":MODIS_Grid_Daily_1km_LST:LST_Day_1km
 #HDF4_EOS:EOS_GRID:"MOD11A1.A2001001.h09v04.005.2006343034412.hdf":MODIS_Grid_Daily_1km_LST:QC_Day
 #modis_subset_layer <- paste("HDF4_EOS:EOS_GRID:",f20,":MODIS_Grid_Daily_1km_LST:LST_Day_1km",sep='')
@@ -59,8 +62,8 @@ modis_subset_layer_LST_Day <- paste("HDF4_EOS:EOS_GRID:",infile_LST[1],":MODIS_G
 modis_subset_layer_LST_QC <- paste("HDF4_EOS:EOS_GRID:",infile_LST[1],":MODIS_Grid_Daily_1km_LST:QC_Day",sep="")
 
 #modis_subset_layer <- file.path(in_dir,modis_subset_layer)
-r <-readGDAL(modis_subset_layer_LST_Day)
-r  <-raster(r)
+r  <- readGDAL(modis_subset_layer_LST_Day)
+r  <- raster(r)
 
 r_qc <-readGDAL(modis_subset_layer_LST_QC)
 r_qc  <-raster(r_qc)
@@ -86,7 +89,7 @@ r_qcl1 <- stack(r_qc0,r_qc2,r_qc3,r_qc65)
 layerNames(r_qcl1) <- c("r_qc0 Good quality","r_qc2 Not produced-Cloud","r_qc3 not produced","r_qc65 LST produced")
 plot(r_qcl1)
 
-###### PART II: Reading and Handling QC flags ######
+### Second : Reading and Handling QC flags ######
 
 ## CONVERSION TO RAW FORMAT: rawToBits,inToBits,packBits: this is in {base} package
 intToBits(65) #integer INT four bytes, not that the notation include 01 for 1
@@ -155,22 +158,134 @@ t44 <- (lapply(f_values$value,function(x){rev(as.integer(intToBits(x)[1:8]))}))
 #f_values$bin_val <- unlist(lapply(f_values$value,function(x){rev(as.integer(intToBits(x)[1:8]))}))
 #This is currently created for LST (see S. Mosher blog)
 
-QC_obj <- create_MODIS_QC_table(LST=TRUE)
+########### PART II: PROCESSING MODIS HDF FILES IN WORKFLOW ##########
+########START  HERE EXAMPLE OF USE OF FUNCTIONS FOR MODIS AND RASTER PROCESSING 
+
+#Step 1: download modis tiles for specified product
+#Step 2: import modis tiles for specified product
+#Step 3: deal with modis flags
+#Step 4: mosaic tiles
+#Step 5: reproject and crop extent to study region
+
+#### Set parameters and arguements
+
+in_dir<- "/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing"
+out_dir<- "/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing"
+setwd(out_dir)
+
+#infile_reg_outline=""  #input region outline defined by polygon: none for Venezuela
+#This is the shape file of outline of the study area                                                      #It is an input/output of the covariate script
+infile_reg_outline <- "/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing/OR83M_state_outline.shp"  #input region outline defined by polygon: Oregon
+#ref_rast_name<-""  #local raster name defining resolution, exent, local projection--. set on the fly?? 
+ref_rast_name<-"/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing/mean_day244_rescaled.rst"  #local raster name defining resolution, exent: oregon
+out_suffix <-"_09202013"
+#MODIS_product <- "MOD11A1.005"
+#start_date <- "2001.01.01"
+#end_date <- "2001.01.05"
+#list_tiles<- c("h08v04","h09v04")
+#out_dir<- "/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing"
+#file_format <- "hdf"
+#product_version <- "5"
+#NA_flag_val <- -9999
+#import_file_format <- ".rst"
+#modis_layer_str1 <- unlist(strsplit(modis_subdataset[1],"\""))[3] #Get day LST layer
+
+##########################################
+#### STEP 1:  DOWNLOAD MODIS PRODUCT  ####
+
+MODIS_product <- "MOD11A1.005"
+start_date <- "2001.01.01"
+end_date <- "2001.01.05"
+list_tiles<- c("h08v04","h09v04")
+out_dir<- "/Users/benoitparmentier/Dropbox/Data/NCEAS/MODIS_processing"
+file_format <- "hdf"
+product_version <- "5"
+#out_suffix <-
+#Note that files are downloaded in the ouput directory in subdirectory with tile_name (e.g. h08v04)
+#debug(modis_product_download)
+
+download_modis_obj <- modis_product_download(MODIS_product,version,start_date,end_date,list_tiles,file_format,out_dir)
+
+####################################
+##### STEP 2: IMPORT MODIS LAYERS ###
+
+#infile_LST <- list_files_tiles #use only hdf!!!
+names(download_modis_obj)
+infile_LST <- download_modis_obj$list_files_by_tiles[,1]
+GDALinfo_hdf<-GDALinfo(infile_LST[1])
+str(GDALinfo_hdf)
+modis_subdataset <- attributes(GDALinfo_hdf)$subdsmdata
+print(modis_subdataset)
+
+modis_layer_str1 <- unlist(strsplit(modis_subdataset[1],"\""))[3] #Get day LST layer
+modis_layer_str2 <- unlist(strsplit(modis_subdataset[3],"\""))[3] #Get day QC layer
+
+#import one file for exploration and checking values
+r_LST <-import_modis_layer_fun(infile_LST[1],subdataset=modis_layer_str1,NA_flag=-9999,out_rast_name="test.rst",memory=T)
+r_qc <-import_modis_layer_fun(infile_LST[1],subdataset=modis_layer_str2,NA_flag=-9999,out_rast_name="test_qc.rst",memory=T)
+plot(r_LST)
+
+### import list of files before mosaicing
+
+file_format_import <- ".rst"
+NA_flag_val=-9999
+out_suffix_s <- "day_LST"
+list_param_import_modis <- list(i=1,hdf_file=infile_LST,subdataset=modis_layer_str1,NA_flag_val=NA_flag_val,out_dir=out_dir,
+                                out_suffix=out_suffix_s,file_format=file_format_import)
+#debug(import_list_modis_layers_fun)
+import_list_modis_layers_fun(1,list_param_import_modis)
+r_LST_s <- lapply(1:length(infile_LST),FUN=import_list_modis_layers_fun,list_param=list_param_import_modis)
+out_suffix_s <- "day_qc"
+list_param_import_modis <- list(i=1,hdf_file=infile_LST,subdataset=modis_layer_str2,NA_flag_val=NA_flag_val,out_dir=out_dir,
+                                out_suffix=out_suffix_s,file_format=file_format_import)
+r_LSTqc_s <- lapply(1:length(infile_LST),FUN=import_list_modis_layers_fun,list_param=list_param_import_modis)
+
+r_LST_s <- unlist(r_LST_s) #list of files as character vector
+r_LSTqc_s <- unlist(r_LSTqc_s) #list of files as character vector
+r_LST_s
+r_LSTqc_s
+
+#################################
+##### STEP 3: APPLY/DEAL WITH QC FLAG  ###
+
+## Get QC information for lST and mask values
+QC_obj <- create_MODIS_QC_table(LST=TRUE, NDVI=FALSE) #Get table corresponding to QC for LST
 names(QC_obj)
 QC_data_lst <- QC_obj$LST
-qc_lst_valid <- subset(x=QC_data_lst,Bit1 == 0 & Bit0 ==1 & Bit3 !=1)
+QC_data_ndvi <- QC_obj$NDVI
+
+#qc_lst_valid <- subset(x=QC_data_lst,Bit1 == 0 & Bit0 ==1 & Bit3 !=1) #only keep Good Quality (QA_word1)
+### Now that QC table has been generated, screen table for desired values
+#Select level 1:
+qc_lst_valid <- subset(x=QC_data_lst,QA_word1 == "LST Good Quality" | QA_word1 =="LST Produced,Check QA")
+#Select level 2:
+qc_lst_valid <- subset(x=qc_lst_valid,QA_word2 == "Good Data" | QA_word2 =="Other Quality")
+#Select level 3:
+#...
+
 names(qc_lst_valid)
-qc_lst_valid$Integer_Value
+qc_valid<-qc_lst_valid$Integer_Value #value integer values
+NA_flag_val <- -9999
+out_rast_name <- "test.tif"
+#debug(qc_valid_modis_fun)
+#Quick look at one image
+#r_stack <- qc_valid_modis_fun(qc_valid,rast_qc=r_qc,rast_var=r_LST,rast_mask=TRUE,NA_flag_val,out_dir=".",out_rast_name)
 
-qc_valid<-qc_lst_valid$Integer_Value
+list_param_screen_qc <- list(qc_valid,r_LSTqc_s, r_LST_s,rast_mask=TRUE,NA_flag_val,out_dir,out_suffix) 
+names(list_param_screen_qc) <- c("qc_valid","rast_qc", "rast_var","rast_mask","NA_flag_val","out_dir","out_suffix") 
 
-qc_valid_modis_fun(qc_valid,rast_qc,rast_var){
+#debug(screen_for_qc_valid_fun)
+r_stack <- screen_for_qc_valid_fun(1,list_param=list_param_screen_qc)
   
-## function to download modis product??
+r_stack <- lapply(1:length(r_LST_s),FUN=screen_for_qc_valid_fun,list_param=list_param_screen_qc)
 
-#add here...
+### Now loop over a series of files...
+
+#################################
+##### STEP 4: MOSAIC TILES  ###
 
 
-
+#################################
+##### STEP 5: REPROJECT AND CROP TO STUDY REGION  ###
 
 
