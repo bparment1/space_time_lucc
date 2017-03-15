c982d4817384d01b6d3ca5b6a7cffa3f8d52e92a
Benoit Parmentier <benoit.parmentier@gmail.com>
2017-03-15 10:45:00 -0400

    modification to record figures for sbt

--- SPatial_analysis_spatial_reg_NDVI_Katrina_case3.R
+++ SPatial_analysis_spatial_reg_NDVI_Katrina_case3.R
@@ -21,7 +21,7 @@
 # - automation to call from the terminal/shell
 #
 #
-#COMMIT: first changes to consider different resolution for sbt
+#COMMIT: modification to record figures for sbt
 #
 #################################################################################################
 
@@ -185,112 +185,142 @@ if(!is.null(agg_fact)){
   l_rast <- lf_agg 
 }
 
-s_raster <- stack(l_rast) #stack with all the variables
-projection(s_raster) <- CRS_reg
-names(s_raster) <- names(data_tb)                
-r_FID <- subset(s_raster,1) #Assumes ID or reference image is the first image of the stack
 
-##Figure 1: reference layer
-
-plot(r_FID,main=l_rast[1])
-head(freq(r_FID))
-
-##Figure 2: zonal layer
-
-plot(subset(s_raster,zonal_colnames),main=zonal_colnames)
-
-reg_var_list <- l_rast[var_names] #only select population raster
-r_stack <- stack(reg_var_list)
-projection(r_stack) <- CRS_reg
-names(r_stack) <- names(data_tb)[var_names]
-
-#Later rerport this basic information in  a text file 
-dim(r_stack) #34x49x230 
-ncell(s_raster) #1666
-freq(r_FID,value=NA) #122
-ncell(s_raster) - freq(r_FID,value=NA) #1544
-res(r_stack) #about 1km
-
-#Automate this step?
-
-## Figure 3: visualization of time series
-
-#projection(r_stack) <- CRS_WGS84 #making sure the same  CRS format is used
-levelplot(r_stack,layers=1:4,col.regions=matlab.like(125)) #show first four images (half a year more or less)
-plot(r_stack,y=1:4)
-
-## Figure 4: visualization of event in time series
-
-n_time_event #108
-dates3[108]
-#[1] "2005-08-29"
-
-## plottting after and before event
-n_before <- n_time_event - 5
-n_after <- n_time_event + 5
-
-levelplot(r_stack,layers=n_before:n_after,col.regions=matlab.like(125))
-
-## Figure 5: visualization of histogram of event in time series
-histogram(subset(r_stack,n_before:n_after))
-
-## Figure 6: Profile for the time series
-
-mean_vals <- colMeans(data_tb[,var_names],na.rm=T)
-pixval <- data_tb[800,var_names]
-#pix300 <- data_tb[300,var_names]
-
-plot(1:length(var_names),mean_vals,type="b",ylab="var",xlab="time step",
-     main="Average variable and pixel 800 profile")
-lines(1:length(var_names),pixval,type="b",ylab="var",xlab="time step",col=c("red"),
-     main="Average variable and pixel 800 profile")
-legend("bottomleft",legend=c("Overall average VAR ","PIX 800 "),
-        col=c("black","red"),lty=c(1,2))
-abline(v=n_time_event,col="blue")
-## Figure 5: visualization of histogram of event in time series
-
-#title("Average pop per elevation zones (observed data)")
-## By zone/strata
-
-r_zonal <- subset(s_raster,zonal_colnames)
-zones_tb_avg<- zonal(r_stack,r_zonal,fun='mean')
-
-zones_avg_df <- as.data.frame(zones_tb_avg)
-n_zones <- length(unique(zones_avg_df$zone))
-
-n_time <- ncol(zones_avg_df) -1
-#pred_names <- c("zone",paste("t",2:n_time,sep="_"),"method")
-
-mydata<- zones_avg_df
-#dd <- do.call(make.groups, mydata[,-ncol(mydata)]) 
-dd <- do.call(make.groups, mydata) 
-#dd$lag <- mydata$lag 
-dd$zones <- mydata$zones
-#dd$method <- mydata$method
-#drop first few rows that contain no data but zones...
-n_start <-n_zones +1
-dd <- dd[n_start:nrow(dd),]
-tmp_time <-unlist(lapply(1:n_time,FUN=function(i){rep(i,n_zones)}))
-dd$time  <- tmp_time
-dd$zones <- mydata$zone #use recycle rule
-
-xyplot(data~time |zones,#group=method,
-       data=dd,type="b",xlab="time",ylab="VAR",
-       #strip = strip.custom(factor.levels=c("z3","z4","z5")), #fix this!!!
-      auto.key = list("topright", corner = c(0,1),# col=c("black","red"),
-                     border = FALSE, lines = TRUE,cex=1.2)
-)
-
-xyplot(data~time,group=zones,
-       data=dd,type="b",xlab="time",ylab="VAR",
-       #strip = strip.custom(factor.levels=c("z3","z4","z5")), #fix this!!!
-      auto.key = list("topright", corner = c(0,1),# col=c("black","red"),
-                     border = FALSE, lines = TRUE,cex=1.2),
-      main="Average by zones for VAR"
-)
-
-write.table(zones_avg_df,file=paste("zones_avg_df","_",out_suffix,".txt",sep=""))
-write.table(dd,file=paste("zones_avg_df_long_table","_",out_suffix,".txt",sep=""))
+explore_and_summarize_data <- function(l_rast,zonal_colnames,var_names,n_time_event){
+  ## function that generates a set of figures and summary of basics information on data
+  #
+  #
+  
+  s_raster <- stack(l_rast) #stack with all the variables
+  projection(s_raster) <- CRS_reg
+  names(s_raster) <- names(data_tb)                
+  r_FID <- subset(s_raster,1) #Assumes ID or reference image is the first image of the stack
+  
+  ##Figure 1: reference layer
+  
+  ##Figure 1: wwf ecoregion
+  res_pix<-960
+  col_mfrow<-1
+  row_mfrow<-1
+  png(filename=paste("Figure1_ref_layer_time_1",out_suffix,".png",sep=""),
+      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+  
+  plot(r_FID,main=l_rast[1])
+  dev.off()
+  
+  
+  freq_tb <- (freq(r_FID))
+  #writeRaster()
+  
+  ##Figure 2: zonal layer
+  
+  res_pix<-960
+  col_mfrow<-1
+  row_mfrow<-1
+  png(filename=paste("Figure1_ref_layer_time_1",out_suffix,".png",sep=""),
+      width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+  
+  plot(subset(s_raster,zonal_colnames),main=zonal_colnames)
+  
+  dev.off()
+  
+  reg_var_list <- l_rast[var_names] #only select population raster
+  r_stack <- stack(reg_var_list)
+  projection(r_stack) <- CRS_reg
+  names(r_stack) <- names(data_tb)[var_names]
+  
+  #Later rerport this basic information in  a text file 
+  dim(r_stack) #34x49x230 
+  ncell(s_raster) #1666
+  freq(r_FID,value=NA) #122
+  ncell(s_raster) - freq(r_FID,value=NA) #1544
+  res(r_stack) #about 1km
+  
+  #Automate this step?
+  
+  ## Figure 3: visualization of time series
+  
+  #projection(r_stack) <- CRS_WGS84 #making sure the same  CRS format is used
+  levelplot(r_stack,layers=1:4,col.regions=matlab.like(125)) #show first four images (half a year more or less)
+  plot(r_stack,y=1:4)
+  
+  ## Figure 4: visualization of event in time series
+  
+  n_time_event #108
+  dates3[108]
+  #[1] "2005-08-29"
+  
+  ## plottting after and before event
+  n_before <- n_time_event - 5
+  n_after <- n_time_event + 5
+  
+  levelplot(r_stack,layers=n_before:n_after,col.regions=matlab.like(125))
+  
+  ## Figure 5: visualization of histogram of event in time series
+  histogram(subset(r_stack,n_before:n_after))
+  
+  ## Figure 6: Profile for the time series
+  
+  mean_vals <- colMeans(data_tb[,var_names],na.rm=T)
+  pixval <- data_tb[800,var_names]
+  #pix300 <- data_tb[300,var_names]
+  
+  plot(1:length(var_names),mean_vals,type="b",ylab="var",xlab="time step",
+       main="Average variable and pixel 800 profile")
+  lines(1:length(var_names),pixval,type="b",ylab="var",xlab="time step",col=c("red"),
+        main="Average variable and pixel 800 profile")
+  legend("bottomleft",legend=c("Overall average VAR ","PIX 800 "),
+         col=c("black","red"),lty=c(1,2))
+  abline(v=n_time_event,col="blue")
+  ## Figure 5: visualization of histogram of event in time series
+  
+  #title("Average pop per elevation zones (observed data)")
+  ## By zone/strata
+  
+  r_zonal <- subset(s_raster,zonal_colnames)
+  zones_tb_avg<- zonal(r_stack,r_zonal,fun='mean')
+  
+  zones_avg_df <- as.data.frame(zones_tb_avg)
+  n_zones <- length(unique(zones_avg_df$zone))
+  
+  n_time <- ncol(zones_avg_df) -1
+  #pred_names <- c("zone",paste("t",2:n_time,sep="_"),"method")
+  
+  mydata<- zones_avg_df
+  #dd <- do.call(make.groups, mydata[,-ncol(mydata)]) 
+  dd <- do.call(make.groups, mydata) 
+  #dd$lag <- mydata$lag 
+  dd$zones <- mydata$zones
+  #dd$method <- mydata$method
+  #drop first few rows that contain no data but zones...
+  n_start <-n_zones +1
+  dd <- dd[n_start:nrow(dd),]
+  tmp_time <-unlist(lapply(1:n_time,FUN=function(i){rep(i,n_zones)}))
+  dd$time  <- tmp_time
+  dd$zones <- mydata$zone #use recycle rule
+  
+  xyplot(data~time |zones,#group=method,
+         data=dd,type="b",xlab="time",ylab="VAR",
+         #strip = strip.custom(factor.levels=c("z3","z4","z5")), #fix this!!!
+         auto.key = list("topright", corner = c(0,1),# col=c("black","red"),
+                         border = FALSE, lines = TRUE,cex=1.2)
+  )
+  
+  xyplot(data~time,group=zones,
+         data=dd,type="b",xlab="time",ylab="VAR",
+         #strip = strip.custom(factor.levels=c("z3","z4","z5")), #fix this!!!
+         auto.key = list("topright", corner = c(0,1),# col=c("black","red"),
+                         border = FALSE, lines = TRUE,cex=1.2),
+         main="Average by zones for VAR"
+  )
+  
+  write.table(zones_avg_df,file=paste("zones_avg_df","_",out_suffix,".txt",sep=""))
+  write.table(dd,file=paste("zones_avg_df_long_table","_",out_suffix,".txt",sep=""))
+  
+ explore_obj <- list(zones_avg,dd)
+ names(explore_obj) <- c("zones_avg","zones_avg_long_tb")
+ return(explore_obj)
+}
 
 ##############################################################################################
 ############## PART V PREDICT MODELS FOR USING TEMP AND SPAT REGRESSION OVER MULTIPLE time steps ####
