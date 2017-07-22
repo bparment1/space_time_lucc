e511655760c77cad981aa80570db876824c6e1a7
bparment1 <benoit.parmentier@gmail.com>
2017-07-22 10:45:00 -0400

    sbt curve figure and raster generation adding temporal and spatial autocorrelation

--- sbt_curve_generation.R
+++ sbt_curve_generation.R
@@ -58,11 +58,13 @@ t_corr_fun(2,list_rast=list_r)
 var_mean <- cellStats(r_var,mean)
 s_corr <- unlist(lapply(list_r, function(rast){Moran(rast,w=f)}))
 t_corr <- unlist(lapply(2:length(list_r),FUN=t_corr_fun,list_rast=list_r))
+t_corr <- c(NA,t_corr)
 plot(var_mean,type="l")
-plot(diff(var_mean))
+## TO find A, B, C: use differencing
+plot(diff(var_mean),type="l")
 
-plot(t_corr,type="l",col="pink",ylim=c(-1,1))
-lines(s_corr,type="l",col="blue")
+plot(t_corr,type="b",col="pink",ylim=c(-1,1))
+lines(s_corr,type="b",col="blue")
 
 par(mar = c(5, 4, 4, 4) + 0.3)  # Leave space for z axis
 plot(t_corr,type="l",col="pink")
@@ -70,6 +72,16 @@ par(new = TRUE)
 plot(s_corr,type="l",col="blue",axes = FALSE, bty="n",xlab = "", ylab = "")
 axis(side=4, at = pretty(range(s_corr)))
 mtext("spat corr", side=4, line=3)
+
+### Generate tables:
+
+df_ts <- data.frame(s_corr=s_corr,t_corr=t_corr,var=var_mean)
+df_ts$time_step <- 1:nrow(df_ts)
+View(df_ts)
+plot(t_corr,s_corr,type="b")
+text(t_corr,s_corr,labels=df_ts$time_step,cex=2)
+#text(c(2,2),c(37,35),labels=c("Non-case","Case"))
+
 
 ################# END OF SCRIPT ##################
 
