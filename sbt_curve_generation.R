ebbf6e1e3a22314d40d90a9a87428dcd4da5a5cb
bparment1 <benoit.parmentier@gmail.com>
2017-07-30 10:45:00 -0400

    sbt curve generation adding documentation and sourcing functions

--- sbt_curve_generation.R
+++ sbt_curve_generation.R
@@ -1,6 +1,48 @@
-library(raster)
+####################################    Space Beats Time Research  #######################################
+################################ Generating datasets for examples of curves #######################################
+#This script generates datasets for the Space Beats Time Framework.
+#
+#AUTHORS: Benoit Parmentier                                             
+#DATE CREATED: 07/28/2017 
+#DATE MODIFIED: 07/30/2017
+#Version: 1
+#PROJECT:  with Marco Millones            
+#
+#COMMENTS: - Generate additional datasets
+#
+#TO DO:
+# - 
+#COMMIT:  
+#
+#################################################################################################
+
+###Loading R library and packages                                                      
+
+library(sp)
+library(rgdal)
+library(spdep)
+library(BMS) #contains hex2bin and bin2hex
+library(bitops)
+library(gtools)
+library(maptools)
+library(parallel)
 library(rasterVis)
-library(colorRamps)
+library(raster)
+library(zoo)  #original times series function functionalies and objects/classes
+library(xts)  #extension of time series objects and functionalities
+library(forecast) #arima and other time series methods
+library(lubridate) #date and time handling tools
+library(colorRamps) #contains matlab.like color palette
+library(rgeos) #spatial analysis, topological and geometric operations e.g. interesect, union, contain etc.
+library(sphet) #spatial analyis, regression eg.contains spreg for gmm estimation
+
+###### Functions used in this script
+
+function_sbt_curve_generation <- "sbt_curve_generation_functions_07302017.R"
+script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"
+source(file.path(script_path,function_sbt_curve_generation))
+
+#####  Parameters and argument set up ###########
 
 
 ###### PART 1: generate dataset ##########
