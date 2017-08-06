630f0f408db0dad6d9068748e8fbb601634b8f5d
bparment1 <benoit.parmentier@gmail.com>
2017-08-06 10:45:00 -0400

    more changes to test sample, adding method options

--- space_and_time_predictions.R
+++ space_and_time_predictions.R
@@ -6,7 +6,7 @@
 #Temporal predictions use OLS with the image of the previous time or the ARIMA method.
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2014 
-#DATE MODIFIED: 08/04/2017
+#DATE MODIFIED: 08/06/2017
 #Version: 3
 #PROJECT: GLP Conference Berlin,YUCATAN CASE STUDY with Marco Millones            
 #PROJECT: Workshop for William and Mary: an intro to geoprocessing with R 
@@ -51,8 +51,8 @@ library(sphet) #spatial analyis, regression eg.contains spreg for gmm estimation
 
 ###### Functions used in this script
 
-function_space_and_time_predictions <- "space_and_time_predictions_functions_07282017.R"
-function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_07282017.R" #PARAM 1
+function_space_and_time_predictions <- "space_and_time_predictions_functions_08062017.R"
+function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_08062017.R" #PARAM 1
 function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_01092016.R" #PARAM 1
 function_data_figures_reporting <- "spatial_analysis_data_figures_reporting_functions_08042017.R" #PARAM 1
 #script_path <- "/home/parmentier/Data/Space_beats_time/sbt_scripts" #path to script #PARAM 2
@@ -74,7 +74,7 @@ args<-commandArgs(TRUE)
 args_table <- args[1]
 
 #args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Katrina_09292017.csv"
-#args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_sample_data1_08042017.csv"
+args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_sample_data1_08042017.csv"
 
 df_args <- read.table(args_table,sep=",",stringsAsFactors = FALSE)
 
@@ -251,6 +251,8 @@ s_raster <- stack(l_rast)
 #method_space <- c("mle","eigen") #estimator <- "mle",estimation_method <- "eigen"
 #re_initialize_arima <- T
 #method_time <- c("arima","arima",re_initialize_arima) # estimator <- "arima",estimation_method <-"arima"
+#method_time <- c(estimator,estimation_method,F)
+method_time <- c("lm","ols",FALSE)
 
 #debug(run_space_and_time_models)
 
