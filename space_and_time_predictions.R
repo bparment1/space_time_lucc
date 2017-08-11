d472dde5d0d416f24e33a4ddfd721afd0d0480a1
bparment1 <benoit.parmentier@gmail.com>
2017-08-11 10:45:00 -0400

    testing arima related changes:space and time predictions on NDVI data

--- space_and_time_predictions.R
+++ space_and_time_predictions.R
@@ -6,7 +6,7 @@
 #Temporal predictions use OLS with the image of the previous time or the ARIMA method.
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2014 
-#DATE MODIFIED: 08/09/2017
+#DATE MODIFIED: 08/11/2017
 #Version: 3
 #PROJECT: GLP Conference Berlin,YUCATAN CASE STUDY with Marco Millones            
 #PROJECT: Workshop for William and Mary: an intro to geoprocessing with R 
@@ -51,8 +51,8 @@ library(sphet) #spatial analyis, regression eg.contains spreg for gmm estimation
 
 ###### Functions used in this script
 
-function_space_and_time_predictions <- "space_and_time_predictions_functions_08092017.R"
-function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_08092017.R" #PARAM 1
+function_space_and_time_predictions <- "space_and_time_predictions_functions_08112017.R"
+function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_08112017.R" #PARAM 1
 function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_01092016.R" #PARAM 1
 function_data_figures_reporting <- "spatial_analysis_data_figures_reporting_functions_08042017.R" #PARAM 1
 #script_path <- "/home/parmentier/Data/Space_beats_time/sbt_scripts" #path to script #PARAM 2
@@ -255,9 +255,9 @@ s_raster <- stack(l_rast)
 #method_time <- c("lm","ols",FALSE)
 
 #debug(run_space_and_time_models)
-function_space_and_time_predictions <- "space_and_time_predictions_functions_08082017.R"
-script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"
-source(file.path(script_path,function_space_and_time_predictions))
+#function_space_and_time_predictions <- "space_and_time_predictions_functions_08082017.R"
+#script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"
+#source(file.path(script_path,function_space_and_time_predictions))
 
 run_space_and_time_models(s_raster,
                           n_time_event,
