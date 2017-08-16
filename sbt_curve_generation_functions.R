35e7199c7c0120c1c785e33dd735e277d9a8a657
bparment1 <benoit.parmentier@gmail.com>
2017-08-16 10:45:00 -0400

    modifying t_corr function to compute temporal correlation to compute on list or raster stack

--- sbt_curve_generation_functions.R
+++ sbt_curve_generation_functions.R
@@ -1,65 +1,15 @@
-####################################    Space Beats Time Research  #######################################
-################################ Generating datasets for examples of curves #######################################
-#This script generates datasets for the Space Beats Time Framework.
-#
-#AUTHORS: Benoit Parmentier                                             
-#DATE CREATED: 07/28/2017 
-#DATE MODIFIED: 08/02/2017
-#Version: 1
-#PROJECT:  with Marco Millones            
-#
-#COMMENTS: - Generate additional datasets
-#
-#TO DO:
-# - 
-#COMMIT:  
-#
-#################################################################################################
-
-###Loading R library and packages                                                      
-
-library(sp)
-library(rgdal)
-library(spdep)
-library(BMS) #contains hex2bin and bin2hex
-library(bitops)
-library(gtools)
-library(maptools)
-library(parallel)
-library(rasterVis)
 library(raster)
-library(zoo)  #original times series function functionalies and objects/classes
-library(xts)  #extension of time series objects and functionalities
-library(forecast) #arima and other time series methods
-library(lubridate) #date and time handling tools
-library(colorRamps) #contains matlab.like color palette
-library(rgeos) #spatial analysis, topological and geometric operations e.g. interesect, union, contain etc.
-library(sphet) #spatial analyis, regression eg.contains spreg for gmm estimation
+library(rasterVis)
+library(colorRamps)
 
-###################### Functions used ############
-
-create_dir_fun <- function(out_dir,out_suffix){
-  #if out_suffix is not null then append out_suffix string
-  if(!is.null(out_suffix)){
-    out_name <- paste("output_",out_suffix,sep="")
-    out_dir <- file.path(out_dir,out_name)
-  }
-  #create if does not exists
-  if(!file.exists(out_dir)){
-    dir.create(out_dir)
-  }
-  return(out_dir)
-}
-
-load_obj <- function(f){
-  env <- new.env()
-  nm <- load(f, env)[1]
-  env[[nm]]
-}
 
 t_corr_fun <- function(i,list_rast){
-  r_subset <- stack(list_rast[[i-1]],list_rast[[i]]) 
-  cor_val<-layerStats(r_subset,'pearson')
+  if(class(list_rast)=="character"){
+    r_subset <- stack(list_rast[[i-1]],list_rast[[i]]) 
+  }else{
+    r_subset <- stack(subset(list_rast,i-1),subset(list_rast,i))
+  }
+  cor_val<-layerStats(r_subset,'pearson',na.rm=T)
   cor_val <- cor_val[[1]][1,2]
   return(cor_val)
 }
@@ -130,12 +80,11 @@ generate_plots_table <- function(r_var,mae_tot_tb,moran_type="queen",out_suffix=
   
   df_ts <- data.frame(s_corr=s_corr,t_corr=t_corr,var=var_mean)
   df_ts$time_step <- 1:nrow(df_ts)
-  #View(df_ts)compute_change_shape_metrics
+  View(df_ts)compute_change_shape_metrics
   plot(t_corr,s_corr,type="b")
   text(t_corr,s_corr,labels=df_ts$time_step,cex=2)
   #text(c(2,2),c(37,35),labels=c("Non-case","Case"))
   
-  return(df_ts)
 }
 
 
