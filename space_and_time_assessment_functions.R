e575642720185c5be8214cd439e27941ba8a6602
bparment1 <benoit.parmentier@gmail.com>
2017-11-07 10:45:00 -0500

    initial commit for space and time assessment

---
+++ space_and_time_assessment_functions.R
@@ -0,0 +1,281 @@
+#######################################    Space Time Analyses Project   #######################################
+############################ Assessing Space and Time Model for Space Beats Time framework #######################################
+#This script contains functions to assess predictions for Space Beats Time Framework.       
+#Temporal predictions use OLS with the image of the previous time step or ARIMA.
+#Spatial predictions use spatial regression (lag error model) with different estimation methods (e.g. eigen, chebyshev etc.).
+#AUTHORS: Benoit Parmentier                                             
+#DATE CREATED: 11/07/2017 
+#DATE MODIFIED: 11/07/2017
+#Version: 1
+
+#PROJECT: Space beats time Framework
+
+#TO DO:
+#
+#COMMIT: initial commit space and time predictions assessment
+#
+
+#################################################################################################
+
+###Loading R library and packages                                                      
+
+library(sp)
+library(rgdal)
+library(spdep)
+library(gtools)
+library(maptools)
+library(parallel)
+library(rasterVis)
+library(raster)
+library(forecast) #ARIMA forecasting
+library(xts)
+library(zoo)
+library(lubridate)
+library(colorRamps) #contains matlab.like color palette
+library(rgeos)
+library(sphet) #contains spreg
+library(BMS) #contains hex2bin and bin2hex
+library(bitops)
+
+###### Functions used in this script
+
+
+
+
+
+
+############ PART V COMPARE MODELS IN PREDICTION ACCURACY #################
+#browser()
+#source(file.path(script_path,function_spatial_regression_analyses)) #source all functions used in this script 1.
+
+#using 11 cores
+#pred_temp_arima <- lapply(1:n_pred,FUN=predict_temp_reg_fun,list_param=list_param_temp_reg)
+
+#pred_temp_arima <- load_obj("temp_reg_obj_arima_arima_t_153_EDGY_predictions_03182015.RData")
+#pred_temp_arima <- load_obj("temp_reg_obj_arima_arima_t_100_NDVI_Katrina_04102015.RData")
+
+#extract_files <- function(i,x){obj<-x[[i]];obj$raster_pred}
+#debug(extract_files)
+#extract_files(1,l_pred_temp_arima)
+if(method_time[1]=="arima"){
+  r_temp_pred_rast_arima <- stack(unlist(lapply(1:length(l_pred_temp_arima),FUN=function(i,x){obj<-x[[i]];obj$raster_pred},x=l_pred_temp_arima)))
+  r_temp_res_rast_arima <- stack(unlist(lapply(1:length(l_pred_temp_arima),FUN=function(i,x){obj<-x[[i]];obj$raster_res},x=l_pred_temp_arima)))
+  levelplot(r_temp_pred_rast_arima,col.regions=rev(terrain.colors(255))) #view the four predictions using mle spatial reg.
+  levelplot(r_temp_res_rast_arima,col.regions=rev(terrain.colors(255))) #view the four predictions using mle spatial reg.
+  projection(r_temp_pred_rast_arima) <- proj_str
+  projection(r_temp_res_rast_arima) <- proj_str
+  temp_pred_rast <- subset(r_temp_pred_rast_arima,1:length(time_window_predicted))
+  
+}
+
+if(method_time[1]=="lm"){
+  r_temp_pred_rast_lm <- stack(lapply(pred_temp_lm,FUN=function(x){x$raster_pred}))
+  r_temp_res_rast_lm <- stack(lapply(pred_temp_lm,FUN=function(x){x$raster_res}))
+  levelplot(r_temp_pred_rast_lm,col.regions=rev(terrain.colors(255))) #view the four predictions using mle spatial reg.
+  levelplot(r_temp_res_rast_lm,col.regions=rev(terrain.colors(255)),main="Var residuals after hurricane")
+  projection(r_temp_res_rast_lm) <- proj_str
+  projection(r_temp_pred_rast_lm) <- proj_str
+  temp_pred_rast <- subset(r_temp_pred_rast_lm,1:length(time_window_predicted))
+  
+}
+
+levelplot(spat_pred_rast_mle_eigen_no_previous,col.regions=rev(matlab.like(255))) #view the four predictions using mle spatial reg.
+levelplot(spat_res_rast_mle_eigen_no_previous,col.regions=rev(matlab.like(255))) #view the four predictions using mle spatial reg.
+
+levelplot(spat_pred_rast_mle_eigen_with_previous,col.regions=matlab.like(255)) #view the four predictions using mle spatial reg.
+levelplot(spat_res_rast_mle_eigen_with_previous,col.regions=matlab.like(255)) #view the four predictions using mle spatial reg.
+
+#spat_pred_rast_mle_eigen_no_previous <- stack(lapply(pred_spat_mle_eigen_no_previous,FUN=function(x){x$raster_pred})) #get stack of predicted images
+#spat_res_rast_mle_eigen_no_previous <- stack(lapply(pred_spat_mle_eigen_no_previous,FUN=function(x){x$raster_res})) #get stack of predicted images
+#levelplot(spat_pred_rast_mle_eigen_no_previous,col.regions=rev(terrain.colors(255))) #view the four predictions using mle spatial reg.
+#levelplot(spat_res_rast_mle_eigen_no_previous,col.regions=matlab.like(25)) #view the four predictions using mle spatial reg.
+
+# Problem with rast_zonal: when aggregated categorical value become continuous
+#
+#
+
+projection(spat_pred_rast_mle_eigen_no_previous) <- proj_str
+projection(spat_res_rast_mle_eigen_no_previous) <- proj_str
+projection(spat_pred_rast_mle_eigen_with_previous) <- proj_str
+projection(spat_res_rast_mle_eigen_with_previous) <- proj_str
+
+#r_huric_obs <- subset(s_raster,time_window_selected[-1]) #remove 99 because not considred in the prediction!!
+r_huric_obs <- subset(s_raster,time_window_predicted) #remove 99 because not considred in the prediction!!
+
+#plot(r_huric_obs)
+#r_huric_w <- crop(r_huric_w,rast_ref)
+levelplot(r_huric_obs,col.regions=matlab.like(25))
+#levelplot(r_temp_pred_rast_arima,col.regions=matlab.like(25))
+
+#r_winds_m <- crop(winds_wgs84,res_temp_s) #small test window
+#res_temp_s_lm <- temp_pred_rast_lm - r_huric_obs
+#res_temp_s_arima <- temp_pred_rast_arima - r_huric_obs
+
+res_temp_s <- temp_pred_rast - r_huric_obs
+res_spat_s <- spat_pred_rast_mle_eigen_no_previous - r_huric_obs
+#res_spat_s <- spat_pred_rast_mle_eigen_no_previous - r_huric_obs
+
+#names(res_temp_s_lm) <- sub("pred","res",names(res_temp_s_lm))
+#names(res_temp_s_arima) <- sub("pred","res",names(res_temp_s_arima))
+names(res_temp_s) <- sub("pred","res",names(res_temp_s))
+
+#names(res_spat_mle_s) <- sub("pred","res",names(res_spat_mle_s))
+names(res_spat_s) <- sub("pred","res",names(res_spat_s))
+#names(res_temp_s) <- paste("r_res_s_",1:nlayers(res_temp_s),"_",out_suffix,sep="")
+
+#r_results <- stack(s_raster,rast_zonal,temp_pred_rast_arima,
+#                   spat_pred_rast_mle_eigen,spat_pred_rast_mle_Chebyshev,res_temp_s_arima,res_temp_s_lm,res_spat_s)
+
+#browser()
+rast_zonal <- subset(s_raster,match(zonal_colnames,names(s_raster)))
+
+#r_results <- stack(s_raster,rast_zonal,temp_pred_rast_arima,
+#                   spat_pred_rast_mle_eigen_no_previous,
+#                   spat_pred_rast_mle_eigen_with_previous,
+#                   res_temp_s_arima,res_spat_s)
+r_results <- stack(s_raster,rast_zonal,temp_pred_rast,
+                   spat_pred_rast_mle_eigen_no_previous,
+                   spat_pred_rast_mle_eigen_with_previous,
+                   res_temp_s,res_spat_s)
+
+dat_out <- as.data.frame(r_results)
+dat_out <- na.omit(dat_out)
+
+filename_dat_out <- file.path(out_dir,paste("dat_out_",out_suffix,".txt",sep=""))
+write.table(dat_out,file=filename_dat_out,
+            row.names=F,sep=",",col.names=T)
+
+### Now accuracy assessment using MAE
+
+method_time
+out_suffix_s <- paste("temp_",method_time[1],"_",out_suffix,sep="")
+
+
+#undebug(calc_ac_stat_fun)
+#ac_temp_arima_obj <- calc_ac_stat_fun(r_pred_s=temp_pred_rast_arima,
+#                                      r_var_s=r_huric_obs,
+#                                      r_zones=rast_zonal,
+#                                      file_format=file_format,
+#                                      out_suffix=out_suffix_s)
+#browser()
+ac_temp_obj <- calc_ac_stat_fun(r_pred_s=temp_pred_rast,
+                                r_var_s=r_huric_obs,
+                                r_zones=rast_zonal,
+                                file_format=file_format,
+                                out_suffix=out_suffix_s)  
+
+#out_suffix_s <- paste("temp_lm_",out_suffix,sep="_")
+
+#ac_temp_lm_obj <- calc_ac_stat_fun(r_pred_s=temp_pred_rast_lm,r_var_s=r_huric_obs,r_zones=rast_zonal,
+#                                file_format=file_format,out_suffix=out_suffix_s)
+
+out_suffix_s <- paste("spat_mle_eigen_with_previous",out_suffix,sep="_")  
+ac_spat_mle_eigen_with_previous_obj <- calc_ac_stat_fun(r_pred_s=spat_pred_rast_mle_eigen_with_previous,
+                                                        r_var_s=r_huric_obs,
+                                                        r_zones=rast_zonal,
+                                                        file_format=file_format,
+                                                        out_suffix=out_suffix_s)
+
+out_suffix_s <- paste("spat_mle_eigen_no_previous",out_suffix,sep="_")  
+ac_spat_mle_eigen_no_previous_obj <- calc_ac_stat_fun(r_pred_s=spat_pred_rast_mle_eigen_no_previous,
+                                                      r_var_s=r_huric_obs,
+                                                      r_zones=rast_zonal,
+                                                      file_format=file_format,
+                                                      out_suffix=out_suffix_s)
+
+#mae_tot_tb <- t(rbind(ac_spat_obj$mae_tb,ac_temp_obj$mae_tb))
+#mae_tot_tb <- (cbind(ac_spat_obj$mae_tb,ac_temp_obj$mae_tb))
+#mae_tot_tb <- (cbind(ac_spat_mle_eigen_no_previous_obj$mae_tb,
+#ac_spat_mle_eigen_with_previous_obj$mae_tb,
+#ac_temp_arima_obj$mae_tb
+#,ac_temp_lm_obj$mae_tb))
+
+mae_tot_tb <- cbind(ac_spat_mle_eigen_no_previous_obj$mae_tb,
+                    ac_spat_mle_eigen_with_previous_obj$mae_tb,
+                    ac_temp_obj$mae_tb)
+
+name_method_time <- paste0("temp_",method_time[1])
+
+mae_tot_tb <- as.data.frame(mae_tot_tb)
+row.names(mae_tot_tb) <- NULL
+names(mae_tot_tb)<- c("spat_reg_no_previous","spat_reg_with_previous",name_method_time)#,"temp_lm")
+#mae_tot_tb$time <- 2:nrow(mae_tot_tb)
+#mae_tot_tb$time <- 2:n_pred
+mae_tot_tb$time <- 1:nlayers(r_huric_obs)
+y_range<- range(cbind(mae_tot_tb$spat_reg_no_previous,mae_tot_tb$spat_reg_with_previous,mae_tot_tb[[name_method_time]]))
+
+res_pix<-960
+col_mfrow<-1
+row_mfrow<-1
+png(filename=paste("Figure_temporal_profiles_time_1_",out_suffix,".png",sep=""),
+    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+
+temp_formula_str <- paste0(paste0("temp_",method_time[1])," ~ ","time")
+plot(spat_reg_no_previous ~ time, type="b",col="cyan",data=mae_tot_tb,ylim=y_range)
+lines(as.formula(temp_formula_str), type="b",col="magenta",data=mae_tot_tb)
+lines(spat_reg_with_previous ~ time, type="b",col="blue",data=mae_tot_tb)
+legend("topleft",
+       legend=c("spat_no",name_method_time,"spat_with"),
+       col=c("cyan","magenta","blue"),
+       lty=1,
+       cex=0.8)
+title("Overall MAE for spatial and temporal models") #Note that the results are different than for ARIMA!!!
+
+dev.off()
+
+write.table(mae_tot_tb,file=paste("mae_tot_tb","_",out_suffix,".txt",sep=""))
+
+#### BY ZONES ASSESSMENT: Ok now it is general so it should be part of the function...
+
+#mae_zones_tb <- rbind(ac_spat_mle_obj$mae_zones_tb[1:3,],
+#                      ac_temp_obj$mae_zones_tb[1:3,])
+mae_zones_tb <- rbind(ac_spat_mle_eigen_no_previous_obj$mae_zones_tb,
+                      ac_spat_mle_eigen_with_previous_obj$mae_zones_tb,
+                      ac_temp_obj$mae_zones_tb)
+
+mae_zones_tb <- as.data.frame(mae_zones_tb)
+
+n_zones <- length(unique(mae_zones_tb$zone))
+
+mae_zones_tb$method <- c(rep("spat_reg_no",n_zones),rep("temp_with_reg",n_zones),rep("temp_arima_reg",n_zones))
+
+n_time <- ncol(mae_zones_tb) -1
+pred_names <- c("zone",paste("t",2:n_time,sep="_"),"method")
+#names(mae_zones_tb) <- c("zones","pred1","pred2","pred3","pred4","method")
+names(mae_zones_tb) <- pred_names
+
+write.table(mae_zones_tb,file=paste("mae_zones_tb","_",out_suffix,".txt",sep=""))
+
+mydata<- mae_zones_tb
+dd <- do.call(make.groups, mydata[,-ncol(mydata)]) 
+#dd$lag <- mydata$lag 
+#drop first few rows that contain no data but zones...
+n_start <-n_zones*3 +1 #3 because we have 3 methods...
+#n_start <-n_zones*2 +1
+dd <- dd[n_start:nrow(dd),]
+
+dd$zones <- mydata$zones
+dd$method <- mydata$method
+
+dd$zones <- mydata$zone #use recycle rule
+
+#Note that to get the title correct, one needs to add
+res_pix<-960
+col_mfrow<-1
+row_mfrow<-1
+png(filename=paste("Figure1_ref_layer_time_1_",out_suffix,".png",sep=""),
+    width=col_mfrow*res_pix,height=row_mfrow*res_pix)
+
+p <- xyplot(data~which | as.factor(zones) ,group=method,data=dd,type="b",xlab="time",ylab="VAR",
+            strip = strip.custom(factor.levels=unique(as.character(dd$zones))), #fix this!!!
+            auto.key = list("topright", corner = c(0,1),# col=c("black","red"),
+                            border = FALSE, lines = TRUE,cex=1.2))
+try(print(p))
+
+dev.off()
+#histogram(r_zonal)
+
+############## Prepare return object #############
+
+space_and_time_prediction_obj <-  list(filename_dat_out,s_raster,mae_tot_tb,mae_zones_tb)
+names(space_and_time_prediction_obj) <- c("filename_dat_out","s_raster","mae_tot_tb","mae_zones_tb")
