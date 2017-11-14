2dec379b5ff72337d22f6190d1cd5caf87ee6634
bparment1 <benoit.parmentier@gmail.com>
2017-11-14 10:45:00 -0500

    adding arguments

--- space_and_time_assessment_functions.R
+++ space_and_time_assessment_functions.R
@@ -5,7 +5,7 @@
 #Spatial predictions use spatial regression (lag error model) with different estimation methods (e.g. eigen, chebyshev etc.).
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 11/07/2017 
-#DATE MODIFIED: 11/13/2017
+#DATE MODIFIED: 11/14/2017
 #Version: 1
 
 #PROJECT: Space beats time Framework
@@ -38,17 +38,56 @@ library(bitops)
 
 #Should use the data that is mosaiced!!
 
+args<-commandArgs(TRUE)
+
+args_table <- args[1]
+
+#args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_10292017.csv"
+args_table <- "/home/parmentier/Data/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_11072017.csv"
+
+df_args <- read.table(args_table,sep=",",stringsAsFactors = FALSE)
+
+### use column 2,3,4 etc.
+#index_val <- 2 #this is set up for parallelization, if we have multiple regions/tiles, tile1
+index_val <- 3 #this is set up for parallelization, if we have multiple regions/tiles, tile 2 rita
+
+in_dir <- df_args[1,index_val]
+out_dir <- df_args[2,index_val]
+proj_str <- df_args[3,index_val]
+file_format <- df_args[4,index_val]
+NA_flag_val <- df_args[5,index_val]
+out_suffix <- df_args[6,index_val]
+create_out_dir_param <- df_args[7,index_val] 
+data_fname <- df_args[8,index_val] 
+coord_names <- df_args[9,index_val]  
+zonal_colnames <- df_args[10,index_val] 
+var_names <- df_args[11,index_val] 
+num_cores <- df_args[12,index_val] 
+n_time_event <- df_args[13,index_val]
+time_window_selected <- df_args[14,index_val] 
+#previous_step <- df_args[15,index_val] 
+date_range <- df_args[15,index_val]  
+r_ref <- df_args[16,index_val]  
+temp_fname <- df_args[17,index_val]
+spat_fname <- df_args[18,index_val]
+method_space <- df_args[19,index_val] 
+method_time <- df_args[20,index_val] 
+pixel_index <- df_args[21,index_val] 
+
+#P1
 in_dir <- "/home/parmentier/Data/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
 r_temp_pred <- list.files(path=in_dir,
                           pattern="r_temp_pred_arima_arima_.*._tile_2_NDVI_Rita_11062017.tif",
                           full.names=T)
-r_spat_pred <- r_temp_pred <- list.files(path=in_dir,
-                                         pattern="r_spat_.*._tile_2_NDVI_Rita_11062017.tif",
-                                         full.names=T)
-r_spat_pred <- r_temp_pred <- list.files(path=in_dir,
+out_file <- "/home/parmentier/Data/Space_beats_time/Data/data_Rita_NDVI/rev_project_output/tile_1/raster_spat_files_list_tile_2.txt"
+write.table(r_temp_pred,out_file)
+#r_spat_pred <- list.files(path=in_dir,
+#                                         pattern="r_spat_.*._tile_2_NDVI_Rita_11062017.tif",
+#                                         full.names=T)
+r_spat_pred <- list.files(path=in_dir,
                                          pattern="r_spat_pred_mle_eigen_no_previous_step_.*._tile_2_NDVI_Rita_11062017.tif",
                                          full.names=T)
-
+out_file <- "/home/parmentier/Data/Space_beats_time/Data/data_Rita_NDVI/rev_project_output/tile_2/raster_spat_files_list_tile_2.txt"
 #r_spat_pred_mle_eigen_no_previous_step__t_113_tile_2_NDVI_Rita_11062017.tif
 s_raster <- "/home/parmentier/Data/Space_beats_time/Data/data_Rita_NDVI/rev_project_output/tile_2/raster_files_list_tile_2.txt"
 date_range <- "2001.01.01;2010.12.31;16"
@@ -65,10 +104,14 @@ create_out_dir <- FALSE
 
 debug(accuracy_space_time_calc)
 test <- accuracy_space_time_calc(r_temp_pred,r_spat_pred,s_raster,time_window_predicted,
-                                     r_zonal,methods_name,r_ref,out_suffix,out_dir,create_out_dir)
+                                     r_zonal,methods_space,method_time,r_ref,out_suffix,
+                                     var_names,NA_flag_val,file_format, date_range,
+                                   out_dir,create_out_dir)
   
 accuracy_space_time_calc <- function(r_temp_pred,r_spat_pred,s_raster,time_window_predicted,
-                                     r_zonal,methods_name,r_ref,out_suffix,out_dir,create_out_dir){
+                                     r_zonal,methods_space,method_time,r_ref,out_suffix,
+                                     var_names,NA_flag_val,file_format,
+                                     out_dir,create_out_dir){
   
   ##INPUTS
   #1)r_temp_pred
@@ -87,11 +130,14 @@ accuracy_space_time_calc <- function(r_temp_pred,r_spat_pred,s_raster,time_windo
   
   ######## Start script #####
   
-  
   ###  Load data if needed:
   if(class(r_temp_pred)=="character"){
     lf <-r_temp_pred 
     r_temp_pred <- stack(lf)
+  }
+  if(class(r_spat_pred)=="character"){
+    lf <-r_spat_pred 
+    r_spat_pred <- stack(lf)
   }
   if(class(s_raster)=="character"){
     df_lf <- read.table(s_raster,sep=",",stringsAsFactors = F,header=T) 
@@ -123,7 +169,6 @@ accuracy_space_time_calc <- function(r_temp_pred,r_spat_pred,s_raster,time_windo
 
   projection(spat_pred_rast_mle_eigen_with_previous) <- proj_str
 
-  
   #### Step 2: compute residuals
   
   res_temp_s <- r_temp_pred - r_obs
@@ -186,11 +231,11 @@ accuracy_space_time_calc <- function(r_temp_pred,r_spat_pred,s_raster,time_windo
   
   temp_formula_str <- paste0(paste0("temp_",method_time[1])," ~ ","time")
   plot(spat_reg_no_previous ~ time, type="b",col="cyan",data=mae_tot_tb,ylim=y_range)
-  lines(as.formula(temp_formula_str), type="b",col="magenta",data=mae_tot_tb)
+  #lines(as.formula(temp_formula_str), type="b",col="magenta",data=mae_tot_tb)
   lines(spat_reg_with_previous ~ time, type="b",col="blue",data=mae_tot_tb)
   legend("topleft",
-         legend=c("spat_no",name_method_time,"spat_with"),
-         col=c("cyan","magenta","blue"),
+         legend=c(namee_method_space,name_method_time),
+         col=c("cyan","blue"),
          lty=1,
          cex=0.8)
   title("Overall MAE for spatial and temporal models") #Note that the results are different than for ARIMA!!!
