5d936f649974828bf7cdb76b6e967a14e05b5cf4
bparment1 <benoit.parmentier@gmail.com>
2017-12-16 10:45:00 -0500

    reading in and computing residuals from space and time predictions

--- space_and_time_assessment.R
+++ space_and_time_assessment.R
@@ -5,7 +5,7 @@
 #Spatial predictions use spatial regression (lag error model) with different estimation methods (e.g. eigen, chebyshev etc.).
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 11/07/2017 
-#DATE MODIFIED: 12/09/2017
+#DATE MODIFIED: 12/16/2017
 #Version: 1
 
 #PROJECT: Space beats time Framework
@@ -59,7 +59,8 @@ load_obj <- function(f){
   env[[nm]]
 }
 
-function_analyses_paper <-"MODIS_and_raster_processing_functions_11172017.R"
+#function_analyses_paper <-"MODIS_and_raster_processing_functions_11172017.R"
+function_analyses_paper <- "space_and_time_assessment_functions_12162017.R"
 script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts"  #path to script functions
 source(file.path(script_path,function_analyses_paper)) #source all functions used in this script.
 
@@ -108,7 +109,11 @@ pixel_index <- df_args[21,index_val]
 mosaic_dir  <- df_args[22,2] #if not null then mosaic based on input by column inputs
 mosaic_out_suffix  <- df_args[23,2] #if not null then mosaic based on input by column inputs
 
+#### Check formatting 
+NA_flag_val <- as.numeric(NA_flag_val)
+
 #### Example of inputs:
+
 #in_dir <- "/home/parmentier/Data/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
 in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_1_NDVI_Rita_11062017"
 in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
@@ -144,6 +149,53 @@ write.table(r_spat_pred,out_file)
 #out_suffix <- "assessment_tile_2_NDVI_Rita_11062017"
 #out_dir <- "output_tile_1_2_combined_NDVI_Rita_11062017"
 #create_out_dir_param <- FALSE  
+if(is.null(r_temp_red)){
+  ## Change this later on to make it general: works for now...
+  ## This should be produce when the predicitons are done
+  
+  in_dir1 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_1_NDVI_Rita_11062017"
+  in_dir2 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
+  
+  r_temp_pred1 <- list.files(path=in_dir1,
+                            pattern="r_temp_pred_arima_arima_.*._tile_1_NDVI_Rita_11062017.tif",
+                            full.names=T)
+  r_temp_pred2 <- list.files(path=in_dir2,
+                            pattern="r_temp_pred_arima_arima_.*._tile_2_NDVI_Rita_11062017.tif",
+                            full.names=T)
+  
+  out_file1 <- file.path(in_dir1,"raster_temp_files_list_tile_1.txt")
+  write.table(r_temp_pred1,out_file1)
+  out_file1 <- file.path(in_dir2,"raster_temp_files_list_tile_2.txt")
+  write.table(r_temp_pred2,out_file2)
+  
+}
+
+if(is.null(r_spat_pred)){
+  ## Change this later on to make it general: works for now...
+  
+  in_dir1 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_1_NDVI_Rita_11062017"
+  in_dir2 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
+  
+  r_spat_pred1 <- list.files(path=in_dir1,
+                                           pattern="r_spat_.*._tile_1_NDVI_Rita_11062017.tif",
+                                           full.names=T)
+  r_spat_pred2 <- list.files(path=in_dir2,
+                                           pattern="r_spat_pred_mle_eigen_no_previous_step_.*._tile_1_NDVI_Rita_11062017.tif",
+                                           full.names=T)
+  #r_spat_pred <- list.files(path=in_dir,
+  #                          pattern="r_spat_pred_mle_eigen_no_previous_step_.*.tif",
+  #                          full.names=T)
+  
+  out_file1 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_1_NDVI_Rita_11062017/raster_spat_files_list_tile_1.txt"
+  out_file2 <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017/raster_spat_files_list_tile_2.txt"
+  
+  write.table(r_spat_pred1,out_file1)
+  write.table(r_spat_pred2,out_file2)
+  #
+}
+
+r_temp_pred <- df_args[17,index_val]
+r_spat_pred <- df_args[18,index_val]
 
 ######################################################
 ########################  BEGIN SCRIPT  #############
@@ -160,6 +212,7 @@ if(create_out_dir_param==TRUE){
 #####################################
 #### STEP 1:  MOSAIC   ####
 
+## if not null then mosaic before the assessment:
 
 if(!is.null(mosaic_dir)){
   
@@ -167,7 +220,7 @@ if(!is.null(mosaic_dir)){
   #out_dir_tmp <- paste0("mosaic_output_",out_suffix)
   out_dir_tmp <- paste0("mosaic_output")
   #out_dir_s <- file.path(out_dir,list_tiles_modis[j])
-  out_dir_s <- mosaic_dir
+  out_dir_s <- file.path(out_dir,mosaic_dir)
   
   if(!file.exists(out_dir_s)){
     dir.create(out_dir_s)
@@ -175,12 +228,6 @@ if(!is.null(mosaic_dir)){
   
   out_dir_mosaic <- out_dir_s
   
-  #list_r_var_s <-list.files(pattern=file_pattern,
-  #                          #patter=".*.NDVI.*.arizona_10182017.rst$",
-  #                         path=in_dir_s,
-  #                         full.names=TRUE) #inputs for moasics
-  
-  ## list to mosaic should be read from input data.frame with all the data
   n_tiles <- ncol(df_args) - 1 
   list_m_var <- vector("list",n_tiles)  
   l_df_raster_name <- vector("list",length=n_tiles) 
@@ -195,52 +242,30 @@ if(!is.null(mosaic_dir)){
   tiles_spat_df <- do.call(cbind,index_tiles_spat)
   #out_suffix <- df_args[6,index_val]
   
-  out_rastnames_temp <- gsub(index_out_suffix[[1]],mosaic_out_suffix,tiles_temp_df[,1])
+  out_rastnames_temp <- gsub(index_out_suffix[[1]],mosaic_out_suffix,basename(tiles_temp_df[,1]))
+  out_rastnames_temp <- gsub(extension(out_rastnames_temp),"",out_rastnames_temp)
   tiles_temp_df$out_rast_names <- out_rastnames_temp
-  out_rastnames_spat <- gsub(index_out_suffix[[1]],mosaic_out_suffix,tiles_spat_df[,1])
+  
+  out_rastnames_spat <- gsub(index_out_suffix[[1]],mosaic_out_suffix,basename(tiles_spat_df[,1]))
+  out_rastnames_temp <- gsub(extension(out_rastnames_spat),"",out_rastnames_spat)
   tiles_spat_df$out_rast_names <- out_rastnames_spat
   
-  #in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_1_NDVI_Rita_11062017"
-  #in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017"
+  ##### First temporal mosaicing
   
-  #r_temp_pred <- list.files(path=in_dir,
-  #                          pattern="r_temp_pred_arima_arima_.*._tile_1_NDVI_Rita_11062017.tif",
-  #                          full.names=T)
-  #out_file <- file.path(in_dir,"raster_temp_files_list_tile_1.txt")
-  #write.table(r_temp_pred,out_file)
-  #r_spat_pred <- list.files(path=in_dir,
-  #                                         pattern="r_spat_.*._tile_1_NDVI_Rita_11062017.tif",
-  #                                         full.names=T)
-  #r_spat_pred <- list.files(path=in_dir,
-  #                                         pattern="r_spat_pred_mle_eigen_no_previous_step_.*._tile_1_NDVI_Rita_11062017.tif",
-  #                                         full.names=T)
-  #r_spat_pred <- list.files(path=in_dir,
-  #                          pattern="r_spat_pred_mle_eigen_no_previous_step_.*.tif",
-  #                          full.names=T)
-  
-  #out_file <- "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017/raster_spat_files_list_tile_2.txt"
-  ##/home/parmentier/Data/Space_beats_time/outputs/output_tile_2_NDVI_Rita_11062017
-  #write.table(r_spat_pred,out_file)
-  
-  #r_temp_pred <- df_args[17,index_val]
-  #r_spat_pred <- df_args[18,index_val]
-  
-  #report on missing dates:
-  #st <- as.Date(start_date,format="%Y.%m.%d")
-  #en <- as.Date(end_date,format="%Y.%m.%d")
-  #ll <- seq.Date(st, en, by="1 day")
-  #dates_queried <- format(ll,"%Y.%m.%d")
-  #date_str <- df_m_mosaics$date
   j <- 1
   
-  out_rastnames_var <- tiles_temp_df$out_rast_names
-  #mosaic_list_var <- tiles_temp_df[,1:n_tiles]
+  #Browse[2]> out_names[j]
+  #[1] "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tiles_combined_NDVI_Rita_11062017/r_temp_pred_arima_arima_105_t_105_tiles_combined_NDVI_Rita_11062017.tif"
+  #out_rastnames_var <- tiles_temp_df$out_rast_names
+  #
+  mosaic_list_var <- tiles_temp_df[,1:n_tiles]
   #xy.list <- split(xy.df, seq(nrow(xy.df)))
   mosaic_list_var <- split(tiles_temp_df[,1:n_tiles], seq(nrow(tiles_temp_df[,1:n_tiles])))
   names(mosaic_list_var) <- NULL
   mosaic_list_var <- lapply(mosaic_list_var,function(x){as.character(x)})
-  input.rasters <- lapply((mosaic_list_var[[1]]), raster)
   
+  #input.rasters <- lapply((mosaic_list_var[[1]]), raster)
+  out_rastnames_var <- out_rastnames_temp
   #list_param_mosaic<-list(j,mosaic_list_var,out_rastnames_var,out_dir,file_format,NA_flag_val)
   list_param_mosaic <- list(j,
                           mosaic_list_var,
@@ -265,24 +290,71 @@ if(!is.null(mosaic_dir)){
   
   #Parallelization
   #started at 22:07 -22:11
-  list_var_mosaiced <-mclapply(1:length(mosaic_list_var), 
+  list_var_mosaiced_temp <-mclapply(1:length(mosaic_list_var), 
                                list_param=list_param_mosaic, 
                                mosaic_m_raster_list,
                                mc.preschedule=FALSE,
                                mc.cores = num_cores) #This is the end bracket from mclapply(...) statement
   
-  #list_var_mosaiced <- lapply(1:length(mosaic_list_var), list_param=list_param_mosaic, mosaic_m_raster_list) #This is the end bracket from mclapply(...) statement
+
+  ############################
+  #### Now spatial variable
   
-  #r_test <- stack(list_var_mosaiced)
-  #plot(r_test,y=1:2)
+  j <- 1
   
+  #Browse[2]> out_names[j]
+  #[1] "/home/bparmentier/Google Drive/Space_beats_time/outputs/output_tiles_combined_NDVI_Rita_11062017/r_temp_pred_arima_arima_105_t_105_tiles_combined_NDVI_Rita_11062017.tif"
+  #out_rastnames_var <- tiles_temp_df$out_rast_names
+  #
+  mosaic_list_var <- tiles_spat_df[,1:n_tiles]
+  #xy.list <- split(xy.df, seq(nrow(xy.df)))
+  mosaic_list_var <- split(tiles_spat_df[,1:n_tiles], seq(nrow(tiles_spat_df[,1:n_tiles])))
+  names(mosaic_list_var) <- NULL
+  mosaic_list_var <- lapply(mosaic_list_var,function(x){as.character(x)})
   
+  #input.rasters <- lapply((mosaic_list_var[[1]]), raster)
+  out_rastnames_var <- out_rastnames_spat
+  #list_param_mosaic<-list(j,mosaic_list_var,out_rastnames_var,out_dir,file_format,NA_flag_val)
+  list_param_mosaic <- list(j,
+                            mosaic_list_var,
+                            out_rastnames_var,
+                            out_dir_mosaic,
+                            file_format,
+                            NA_flag_val)
+  
+  names(list_param_mosaic)<-c("j",
+                              "mosaic_list",
+                              "out_rastnames",
+                              "out_path",
+                              "file_format",
+                              "NA_flag_val")
+  #debug(mosaic_m_raster_list)
+  list_var_mosaiced <- mosaic_m_raster_list(1,list_param_mosaic)
+  #list_var_mosaiced <-mclapply(1:11, 
+  #                             list_param=list_param_mosaic, 
+  #                             mosaic_m_raster_list,
+  #                             mc.preschedule=FALSE,
+  #                             mc.cores = num_cores) #This is the end bracket from mclapply(...) statement
+  
+  #Parallelization
+  #started at 22:07 -22:11
+  list_var_mosaiced_spat <-mclapply(1:length(mosaic_list_var), 
+                                    list_param=list_param_mosaic, 
+                                    mosaic_m_raster_list,
+                                    mc.preschedule=FALSE,
+                                    mc.cores = num_cores) #This is the end bracket from mclapply(...) statement
+  
+  ### Plot results
+  r_spat_m <- stack(unlist(list_var_mosaiced_spat))
+  plot(r_spat_m)
+  r_temp_m <- stack(unlist(list_var_mosaiced_temp))
+  plot(r_temp_m)
 }
 
 #####################################
 #### STEP 2:  ACCURACY ASSESSMENT   ####
 
-debug(accuracy_space_time_calc)
+#debug(accuracy_space_time_calc)
 
 test <- accuracy_space_time_calc(r_temp_pred=r_temp_pred,
                                  r_spat_pred=r_spat_pred,
@@ -301,4 +373,4 @@ test <- accuracy_space_time_calc(r_temp_pred=r_temp_pred,
                                  create_out_dir_param=create_out_dir_param)
   
 
-#################### END OF SCRIPT ################################################################
+###################################### END OF SCRIPT ################################################################
