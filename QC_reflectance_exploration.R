b30dbff71ecdcb2b91a578f6719c9dbc461d2e56
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-02-21 10:45:00 -0500

    function to convert decimal to integer unsigned 32

--- QC_reflectance_exploration.R
+++ QC_reflectance_exploration.R
@@ -5,7 +5,7 @@
 #
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 11/07/2017 
-#DATE MODIFIED: 12/16/2017
+#DATE MODIFIED: 02/21/2018
 #Version: 1
 
 #PROJECT: Space beats time Framework
@@ -26,13 +26,65 @@ require(RCurl)
 require(stringr)
 require(XML)
 library(lubridate)
+library(miscFuncs) #contains binary/bit processing helper functions
+library(data.table)
+
+###### Functions used in this script sourced from other files
+
+#function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_11242015_functions.R" #PARAM 1
+function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_02102018.R" #PARAM 1
+function_space_and_time_assessment <- "space_and_time_assessment_functions_02202018.R" #PARAM 1
+
+script_path <- "/home/bparmentier/Google Drive/Space_beats_time/sbt_scripts" #path on bpy50 #PARAM 2
+#script_path <- "/home/parmentier/Data/Space_beats_time/sbt_scripts" #path on Atlas
+#source(file.path(script_path,function_spatial_regression_analyses)) #source all functions used in this script 1.
+source(file.path(script_path,function_paper_figures_analyses)) #source all functions used in this script 1.
+source(file.path(script_path,function_space_and_time_assessment)) #source all functions used in this script 1.
+
+#zonal stratum for NLU
+##### Functions used in this script 
+
+create_dir_fun <- function(out_dir,out_suffix){
+  if(!is.null(out_suffix)){
+    out_name <- paste("output_",out_suffix,sep="")
+    out_dir <- file.path(out_dir,out_name)
+  }
+  #create if does not exists
+  if(!file.exists(out_dir)){
+    dir.create(out_dir)
+  }
+  return(out_dir)
+}
+
+convert_decimal_to_uint32 <- function(x){
+  bin_val <- bin(x)
+  bin_val <- as.numeric(as.logical(bin_val))
+  
+  if(length(bin_val)< 32){
+    add_zero <- 32-length(bin_val) 
+  }
+  
+  bin_val <- c(rep(0,add_zero),bin_val)
+  
+  return(bin_val)
+}
+
+convert_to_decimal <- function(bin_val){
+  n <- length(bin_val)
+  vals<- lapply(1:n,function(i,coef){coef[i]*2^(n-i)},coef=bin_val)
+  sum(unlist(vals))
+}
+
+#####  Parameters and argument set up ###########
+
+#ARG1: input dir
+in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/Data/data_RITA_reflectance/import_h09v06/"
 
 ## PRODUCT 3: Reflectance
 # This is for MOD09
 #
 #https://lpdaac.usgs.gov/dataset_discovery/modis/modis_products_table/mod09a1
 #This is 32 bits
-in_dir <- "/home/bparmentier/Google Drive/Space_beats_time/Data/data_RITA_reflectance/import_h09v06/"
 r_qc_s1 <- raster(file.path(in_dir,"MOD09A1_A2005001_h09v06_006_sur_refl_qc_500m.tif"))
 r_var_s1 <- raster(file.path(in_dir,"MOD09A1_A2005009_h09v06_006_reflectance.tif"))
 
@@ -55,14 +107,13 @@ writeRaster(r_qc_s1,
             datatype=data_type_str)
 r <- raster(file.path(out_dir_s,raster_name_tmp))
 unique_vals_test <- unique(r)
-convert
 #The flags are 32 bits int
 #library(binaryLogic)
 unique_vals <- unique(r_qc_s1) #first get unique values
 unique_vals==unique_vals_test
 #convert decimal to intu32
 bin(255)
-1*2^7 + 1*2^6 + 1*2^5 + 1*2^4 + 1*2^3 + 1*2^2 + + 1*2^1 + + 1*2^0 
+1*2^7 + 1*2^6 + 1*2^5 + 1*2^4 + 1*2^3 + 1*2^2 + 1*2^1 + 1*2^0 
 length(bin(255))
 
 bin(61)
@@ -100,24 +151,6 @@ bin(61)
 #add_zero <- 2
 #rep(0,add_zero)
 
-convert_decimal_to_uint32 <- function(x){
-  bin_val <- bin(x)
-  bin_val <- as.numeric(as.logical(bin_val))
-  
-  if(length(bin_val)< 32){
-    add_zero <- 32-length(bin_val) 
-  }
-  
-  bin_val <- c(rep(0,add_zero),bin_val)
-  
-  return(bin_val)
-}
-
-convert_to_decimal <- function(bin_val){
-  n <- length(bin_val)
-  vals<- lapply(1:n,function(i,coef){coef[i]*2^(n-i)},coef=bin_val)
-  sum(unlist(vals))
-}
 #debug(convert_to_decimal)
 test
 bin(test)
@@ -127,32 +160,53 @@ test
 rep(NA,64)
 #qc_table_modis <- data.table(bitNo=NA,param_name=NA,BitComb=NA,Sur_refl_qc_500m=NA)
 qc_table_modis <- data.frame(bitNo=NA,param_name=NA,BitComb=NA,Sur_refl_qc_500m=NA)
-
-View(qc_table_modis)
-
-qc_table_modis[1:4,c("bitNo")] <- rep("0-1",4) 
-qc_table_modis[1:4,c("param_name")] <- rep("MODLAND QA bits",4) 
-qc_table_modis[1:4,c("BitComb")] <- c("00","01","10","11") 
-qc_table_modis[1:4,c("Sur_refl_qc_500m")] <- c("corrected product produced at ideal quality all bands",
-                                              "corrected product produced at less than ideal quality some or all bands",
-                                              "corrected product not produced due to cloud effects all bands",
-                                              "corrected producted not produced due to other reasons some or all bands may be fill value [Note that a value of (11) overrides a value of (01)]") 
-
+### Make this a function
 ### now add band quality
 band_no <- 1
-qc_table_modis[5:13,c("bitNo")] <- rep("26-29",9) 
-qc_table_modis[5:13,c("param_name")] <- rep(paste("band",band_no,"quality four bit range",9)) 
-qc_table_modis[5:13,c("BitComb")] <- c("0000","1000","1001","1010","1011","1100","1101","1110","1111") 
-qc_table_modis[5:13,c("Sur_refl_qc_500m")] <- c("highest quality",
-                                               "dead detector; data interpolated in L1B",
-                                               "solar zenith >=86 degrees",
-                                               "solar zenith >=85 degrees and < 86 degrees",
-                                               "missing input",
-                                               "internal constant used in place of climatological data for at least one atmospheric constant",
-                                               "correction out of bounds pixel constrained to extreme allowable value",
-                                               "L1B data faulty",
-                                               "not processed due to deep ocean or clouds") 
+list_band_no <- 1:7
+list_qc_table_modis <- vector("list",length=length(list_band_no))
+start_bit <- 0
+i <- 1
+for(i in 1:length(list_band_no)){
+  band_no <- list_band_no[i]
+  if(i==1){
+    start_bit <- 2
+  }else{
+    start_bit <- end_bit + 1
+  }
+  #start_bit <- 2
+  end_bit <- start_bit + 4 
+  bit_range <- paste(start_bit,end_bit,sep="-")
+  qc_table_modis[1:9,c("bitNo")] <- rep(bit_range,9) 
+  
+  qc_table_modis[1:9,c("param_name")] <- rep(paste("band",band_no,"quality four bit range"),9) 
+  qc_table_modis[1:9,c("BitComb")] <- c("0000","1000","1001","1010","1011","1100","1101","1110","1111") 
+  qc_table_modis[1:9,c("Sur_refl_qc_500m")] <- c("highest quality",
+                                                  "dead detector; data interpolated in L1B",
+                                                  "solar zenith >=86 degrees",
+                                                  "solar zenith >=85 degrees and < 86 degrees",
+                                                  "missing input",
+                                                  "internal constant used in place of climatological data for at least one atmospheric constant",
+                                                  "correction out of bounds pixel constrained to extreme allowable value",
+                                                  "L1B data faulty",
+                                                  "not processed due to deep ocean or clouds") 
+  list_qc_table_modis[[i]] <- qc_table_modis
+}
 
+qc_modis_table_bands <- do.call(rbind,list_qc_table_modis)
+View(qc_modis_table_bands)
+
+qc_table_modis_modland <- data.frame(bitNo=NA,param_name=NA,BitComb=NA,Sur_refl_qc_500m=NA)
+
+qc_table_modis_modland[1:4,c("bitNo")] <- rep("0-1",4) 
+qc_table_modis_modland[1:4,c("param_name")] <- rep("MODLAND QA bits",4) 
+qc_table_modis_modland[1:4,c("BitComb")] <- c("00","01","10","11") 
+qc_table_modis_modland[1:4,c("Sur_refl_qc_500m")] <- c("corrected product produced at ideal quality all bands",
+                                               "corrected product produced at less than ideal quality some or all bands",
+                                               "corrected product not produced due to cloud effects all bands",
+                                               "corrected producted not produced due to other reasons some or all bands may be fill value [Note that a value of (11) overrides a value of (01)]") 
+
+qc_table_modis <- rbind(qc_table_modis_modland,qc_modis_table_bands)
 View(qc_table_modis)
 
 
