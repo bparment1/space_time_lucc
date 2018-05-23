2cb65e7782e5093afc1171001ab49f3c7181b383
Benoit Parmentier <benoit.parmentier@gmail.com>
2018-05-23 10:45:00 -0400

    ita predictions for tile 2 region: first attempt

--- space_and_time_predictions.R
+++ space_and_time_predictions.R
@@ -7,7 +7,7 @@
 # Event type: Rita from 09/18 to 09/26
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2014 
-#DATE MODIFIED: 11/03/2017
+#DATE MODIFIED: 12/20/2017
 #Version: 3
 #PROJECT: GLP Conference Berlin,YUCATAN CASE STUDY with Marco Millones            
 #PROJECT: Workshop for William and Mary: an intro to geoprocessing with R 
@@ -23,7 +23,7 @@
 # - automation to call from the terminal/shell
 #
 #
-#COMMIT: modifications to arima outputs on Katrina NDVI 
+#COMMIT: Rita predictions for tile 2 region: first attempt
 #
 
 #Rscript space_and_time_predictions_07292017b.R "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Katrina_09292017.csv"
@@ -53,8 +53,8 @@ library(sf)
 
 ###### Functions used in this script
 
-function_space_and_time_predictions <- "space_and_time_predictions_functions_11032017.R"
-function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_11032017.R" #PARAM 1
+function_space_and_time_predictions <- "space_and_time_predictions_functions_11072017.R"
+function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_11072017.R" #PARAM 1
 function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_01092016.R" #PARAM 1
 function_data_figures_reporting <- "spatial_analysis_data_figures_reporting_functions_08042017.R" #PARAM 1
 script_path <- "/home/parmentier/Data/Space_beats_time/sbt_scripts" #path to script #PARAM 2
@@ -76,12 +76,13 @@ args<-commandArgs(TRUE)
 args_table <- args[1]
 
 #args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_10292017.csv"
-args_table <- "/home/parmentier/Data/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_11022017.csv"
+args_table <- "/home/parmentier/Data/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_11072017.csv"
 
 df_args <- read.table(args_table,sep=",",stringsAsFactors = FALSE)
 
 ### use column 2,3,4 etc.
-index_val <- 2 #this is set up for parallelization, if we have multiple regions/tiles
+#index_val <- 2 #this is set up for parallelization, if we have multiple regions/tiles, tile1
+index_val <- 3 #this is set up for parallelization, if we have multiple regions/tiles, tile 2 rita
 
 in_dir <- df_args[1,index_val]
 out_dir <- df_args[2,index_val]
