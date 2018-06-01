1a036f7e0c68ae6bf7b25116a32cc1e6bd176575
Benoit Parmentier <benoit.parmentier@gmail.com>
2018-06-01 10:45:00 -0400

    debugging aggregation for categorical variables

--- space_and_time_predictions.R
+++ space_and_time_predictions.R
@@ -7,7 +7,7 @@
 # Event type: Hurricanes, Urbanization etc.
 #AUTHORS: Benoit Parmentier                                             
 #DATE CREATED: 03/09/2014 
-#DATE MODIFIED: 05/31/2018
+#DATE MODIFIED: 06/01/2018
 #Version: 3
 #PROJECT: GLP Conference Berlin,YUCATAN CASE STUDY with Marco Millones            
 #PROJECT: Workshop for William and Mary: an intro to geoprocessing with R 
@@ -88,7 +88,7 @@ library(snow)
 ###### Functions used in this script
 
 ## space beats time predictions run on specific dataset
-function_space_and_time_predictions <- "space_and_time_predictions_functions_05312018.R"
+function_space_and_time_predictions <- "space_and_time_predictions_functions_06012018.R"
 function_space_and_time_assessment <- "space_and_time_assessment_functions_05312018.R"
 function_spatial_regression_analyses <- "SPatial_analysis_spatial_reg_functions_11072017.R" #PARAM 1
 function_paper_figures_analyses <- "space_beats_time_sbt_paper_figures_functions_01092016.R" #PARAM 1
@@ -102,9 +102,8 @@ source(file.path(script_path,function_paper_figures_analyses)) #source all funct
 source(file.path(script_path,function_data_figures_reporting)) #source all functions used in this script 1.
 
 #Aggregation code
-function_multilabel_fuzzy_analyses <- "classification_multilabel_processing_functions_03142017.R" #PARAM 1
-#script_path <- "/home/bparmentier/Google Drive/LISER_Lux/R_scripts" #path to script #PARAM 2
-source(file.path(script_path,function_multilabel_fuzzy_analyses)) #source all functions used in this script 1.
+function_aggregation_raster <- "aggregation_raster_functions_06012018.R" #PARAM 1
+source(file.path(script_path,function_aggregation_raster)) #source all functions used in this script 1.
 
 #####  Parameters and argument set up ###########
 
@@ -115,7 +114,7 @@ args_table <- args[1]
 ###Comment this out if run from shell script
 #args_table <- "/home/bparmentier/Google Drive/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Rita_10292017.csv"
 #args_table <- "/media/dan/Space_beats_time/Space_beats_time/Data/input_arguments_sbt_script_REACT_Lagos_NDVI_mod13_05242018.csv"
-args_table <- "/media/dan/Space_beats_time/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Katrina_05302018.csv"
+args_table <- "/media/dan/Space_beats_time/Space_beats_time/Data/input_arguments_sbt_script_NDVI_Katrina_06012018.csv"
 
 df_args <- read.table(args_table,sep=",",stringsAsFactors = FALSE)
 
@@ -209,6 +208,11 @@ if(zonal_colnames=="NULL"){
 ### reference image
 if(rast_ref=="NULL"){
   rast_ref <- NULL
+}
+
+### reference image
+if(use_majority=="TRUE"){
+  agg_method_cat <- "majority"
 }
 
 #set up the working directory
@@ -336,18 +340,20 @@ if(is.null(rast_ref)){
 
 if(!is.null(agg_fact)){
   #undebug(aggregate_raster_fun)
-  
+  #list_l_rast <- list(l_rast)
+  names(l_rast) <- sub(extension(l_rast),"",basename(l_rast))
   #undebug(aggregate_raster_fun)
-  obj <- aggregate_raster_fun(l_rast,
-                              zonal_colnames,
-                              use_majority,
-                              agg_fact,
-                              agg_fun,
-                              file_format,
-                              rast_ref,
-                              num_cores,
-                              out_suffix,
-                              out_dir)
+
+  obj <- aggregate_raster_fun(l_rast=l_rast,
+                              cat_names=zonal_colnames,
+                              agg_method_cat=use_majority,
+                              agg_fact=agg_fact,
+                              agg_fun=agg_fun,
+                              file_format=file_format,
+                              rast_ref=rast_ref,
+                              num_cores=num_cores,
+                              out_suffix=out_suffix,
+                              out_dir=out_dir)
 
   l_rast <- obj$l_rast  
   zonal_colnames <- obj$zonal_colnames
